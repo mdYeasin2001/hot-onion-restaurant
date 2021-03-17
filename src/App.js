6fda7beb4ba61059b0e0b968449dca26f71b76d9
Yeasin <yeasin30112001@gmail.com>
2021-03-17 15:19:38 -0600

    header and banner and navbar design completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Header from './components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Header/>
+    </Router>
   );
 }
 
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="col-md-6">
+                <h1 className="text-center display-5 pb-3">Best food waiting for your belly</h1>
+                <div className="px-5">
+                <div className="input-group px-5">
+                    <input type="text" className="form-control" placeholder="Search for food items" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <button className="btn btn-danger" type="button" id="button-addon2">Search</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
